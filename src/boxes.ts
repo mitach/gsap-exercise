3acca004ba8ede1511bc8957d5ba7545dd97141c
Dimitar Malinov <85792514+mitach@users.noreply.github.com>
2023-01-17 14:47:30 +0200

    2: Animating Properties

---
+++ src/boxes.ts
@@ -0,0 +1,63 @@
+import * as PIXI from 'pixi.js';
+import { gsap } from 'gsap';
+import { PixiPlugin } from 'gsap/PixiPlugin';
+
+gsap.registerPlugin(PixiPlugin);
+PixiPlugin.registerPIXI(PIXI);
+
+const app = new PIXI.Application({
+    width: 800,
+    height: 600,
+    backgroundColor: 0x999999
+});
+
+document.body.appendChild(app.view as HTMLCanvasElement);
+
+const grid = new PIXI.Graphics();
+grid.lineStyle({ color: 0xffffff, width: 1, alpha: 0.2 });
+
+for (let x = 0; x < 800; x += 50) {
+    if ((x - 50) % 200 == 0) {
+        grid.line.alpha = 0.5;
+    } else {
+        grid.line.alpha = 0.2;
+    }
+    grid.moveTo(x, 0);
+    grid.lineTo(x, 600);
+}
+for (let y = 0; y < 600; y += 50) {
+    if ((y - 50) % 200 == 0) {
+        grid.line.alpha = 0.5;
+    } else {
+        grid.line.alpha = 0.2;
+    }
+    grid.moveTo(0, y);
+    grid.lineTo(800, y);
+}
+
+app.stage.addChild(grid);
+
+const sq1 = new PIXI.Graphics();
+sq1.beginFill(0xffffff);
+sq1.drawRect(100, 300, 100, 100);
+sq1.endFill();
+
+const sq2 = new PIXI.Graphics();
+sq2.beginFill(0xffffff);
+sq2.drawRect(300, 300, 100, 100);
+sq2.endFill();
+
+const sq3 = new PIXI.Graphics();
+sq3.beginFill(0xffffff);
+sq3.drawRect(500, 300, 100, 100);
+sq3.endFill();
+
+const sq4 = new PIXI.Graphics();
+sq4.beginFill(0xffffff);
+sq4.drawRect(700, 300, 100, 100);
+sq4.endFill();
+
+gsap.to([sq1, sq2, sq3, sq4], {pixi: {rotation: 360, blur: 10, skewX: 50, tint: 0xff0000}, duration: 2, delay: 1});
+// gsap.to([sq1, sq2, sq3, sq4], {rotation: 360, blur: 10, skewX: 50, tint: 0xff0000, duration: 2, delay: 1});
+
+app.stage.addChild(sq1, sq2, sq3, sq4);
